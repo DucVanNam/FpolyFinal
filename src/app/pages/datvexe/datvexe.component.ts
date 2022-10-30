108841be9476fbeac301acc2756ec99b795c6a58
OFFICEVNPAY\namldv <subinducnam@gmail.com>
2022-10-30 15:08:54 +0700

    getallbyroute

--- src/app/pages/datvexe/datvexe.component.ts
+++ src/app/pages/datvexe/datvexe.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { DatVeXeService } from './datvexe.service';
 interface AllByRoute{
-  id: string,
-  price: Number;
+  coachOwner: string;
+  emptySeats: number;
+  from: string;
+  to: string;
+  numberOfSeats: number;
+  ticketPrice: string;
+  schedule: string;
 }
 @Component({
   selector: 'app-datvexe',
@@ -12,63 +17,6 @@ interface AllByRoute{
 export class DatvexeComponent{
   array = ['Chúng tôi sẽ đưa đến cho bạn một trải nghiệm tốt nhất'];
   datas:AllByRoute[]=[];
-  data = [
-    {
-      id: 1,
-      name: 'Nhà xe test',
-      diemdi: 'Hà Nội',
-      diemtoi: 'Lào Cai',
-      loaixe: '16 chỗ',
-      giave: '100000',
-      giodi: '12:00',
-      gioden: '13:50',
-      ghetrong: 10
-    },
-    {
-      id: 2,
-      name: 'Nhà xe xác ướp ai cập',
-      diemdi: 'Hà Nội',
-      diemtoi: 'Bắc Giang',
-      loaixe: '30 chỗ',
-      giave: '50000',
-      giodi: '12:00',
-      gioden: '13:50',
-      ghetrong: 10
-    },
-    {
-      id: 3,
-      name: 'Nhà xe hihihaha',
-      diemdi: 'Hà Nội',
-      diemtoi: 'Hải Phòng',
-      loaixe: '30 chỗ',
-      giave: '70000',
-      giodi: '12:00',
-      gioden: '13:50',
-      ghetrong: 10
-    },
-    {
-      id: 4,
-      name: 'Chuyến xe số 4',
-      diemdi: 'Hà Nội',
-      diemtoi: 'Quảng Ninh',
-      loaixe: '16 chỗ',
-      giave: '100000',
-      giodi: '12:00',
-      gioden: '13:50',
-      ghetrong: 10
-    },
-    {
-      id: 5,
-      name: 'Chuyến xe số 5',
-      diemdi: 'Hà Nội',
-      diemtoi: 'Cần Thơ',
-      loaixe: '30 chỗ',
-      giave: '90000',
-      giodi: '12:00',
-      gioden: '13:50',
-      ghetrong: 10
-    }
-  ];
   data2 = [
     {
       id: 1,
@@ -139,6 +87,7 @@ export class DatvexeComponent{
   date = null;
 
   isVisible = false;
+  isVisible2 = false;
 
   selectedValue = null;
 
@@ -180,11 +129,13 @@ export class DatvexeComponent{
   handleOk(): void {
     console.log('Button ok clicked!');
     this.isVisible = false;
+    this.isVisible2 = false;
   }
 
   handleCancel(): void {
     console.log('Button cancel clicked!');
     this.isVisible = false;
+    this.isVisible2 = false;
   }
   onChange(value: string): void {
     this.filteredOptions = this.options.filter(option => option.toLowerCase().indexOf(value.toLowerCase()) !== -1);
@@ -199,6 +150,13 @@ export class DatvexeComponent{
     }
     this.routeService.getAllByRoute(params).subscribe((res: any)=>{
        this.datas = res;
+       console.log('data', res);
+       console.log('data', this.datas);
     })
+  }
+
+  thongtin(item: any) {
+    console.log('data', item);
+    this.isVisible2 = true;
   }
 }
