87c2fde3aa0e4a01c0f7000af214856bd8616ef2
OFFICEVNPAY\namldv <subinducnam@gmail.com>
2022-11-13 02:14:08 +0700

    crud layout

--- src/app/pages/quanlylayout/quanlylayout.component.ts
+++ src/app/pages/quanlylayout/quanlylayout.component.ts
@@ -1,81 +1,122 @@
 import { Component, OnInit } from '@angular/core';
+import { QuanLyLayoutService } from './quanlylayout.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
 
-interface Person {
-  key: string;
-  Name: string;
-  Value: string;
-  NumberOfDeckers: string;
+interface Layout {
+  name: string;
+  value: string;
+  numberOfDeckers: Number;
 }
 @Component({
   selector: 'app-quanlylayout',
   templateUrl: './quanlylayout.component.html',
   styleUrls: ['./quanlylayout.component.scss']
 })
-export class QuanlylayoutComponent {
+export class QuanlylayoutComponent implements OnInit {
+  //true tạo, false sửa
+  iscreup : boolean = true;
+  confirmModal?: NzModalRef;
   date = null;
-  constructor() { }
-  listOfData: Person[] = [
-    {
-      key: '1',
-      Name: 'Gi được nhỉ',
-      Value: '123',
-      NumberOfDeckers: '1'
-    },
-    {
-      key: '1',
-      Name: 'Gi được nhỉ',
-      Value: '123',
-      NumberOfDeckers: '1'
-    },
-    {
-      key: '1',
-      Name: 'Gi được nhỉ',
-      Value: '123',
-      NumberOfDeckers: '1'
-    },
-    {
-      key: '1',
-      Name: 'Gi được nhỉ',
-      Value: '123',
-      NumberOfDeckers: '1'
-    },
-    {
-      key: '1',
-      Name: 'Gi được nhỉ',
-      Value: '123',
-      NumberOfDeckers: '1'
-    },
-    {
-      key: '1',
-      Name: 'Gi được nhỉ',
-      Value: '123',
-      NumberOfDeckers: '1'
-    },
-    {
-      key: '1',
-      Name: 'Gi được nhỉ',
-      Value: '123',
-      NumberOfDeckers: '1'
-    },
-    {
-      key: '1',
-      Name: 'Gi được nhỉ',
-      Value: '123',
-      NumberOfDeckers: '1'
-    }
-  ];
+  constructor(private layoutService : QuanLyLayoutService, public fb: FormBuilder, private message: NzMessageService, private modal: NzModalService) { }
+  listOfData: Layout[]=[];
   isVisible = false;
+  validateForm!: FormGroup;
+  listOfFormDto = {
+    id: null,
+    name : null,
+    value: null,
+    numberOfDeckers: 0
+  }
+
+  ngOnInit(): void {
+    this.getall();
+    this.validateForm = this.fb.group({
+      name: [null, [Validators.required]],
+      value: [null, [Validators.required]],
+      numberofdeckers: [null, [Validators.required]],
+    });
+  }
+
+  successMessage(): void {
+    this.message.create('success', 'Tạo mới thành công');
+  }
+  successDeleteMessage(): void {
+    this.message.create('success', 'Xóa bản ghi thành công');
+  }
+  errorMessage(): void {
+    this.message.create('error', 'Có lỗi xảy ra trong quá trình tạo');
+  }
+
   showModal(): void {
     this.isVisible = true;
+    this.iscreup = true;
   }
-  handleOk(): void {
-    console.log('Button ok clicked!');
-    this.isVisible = false;
+  create(): void {
+    if(this.validateForm.valid){
+      let params = {
+        name: this.listOfFormDto.name,
+        value: this.listOfFormDto.value,
+        numberOfDeckers: this.listOfFormDto.numberOfDeckers,
+      };
+      this.layoutService.create(params).subscribe((res: any) => {
+        if (res) {
+          this.successMessage();
+          this.handleCancel();
+          this.getall();
+        }
+        else{
+          this.errorMessage();
+        }
+      });
+      }
+    else{
+      Object.values(this.validateForm.controls).forEach(control =>{
+        if(control.invalid){
+          control.markAsDirty();
+          control.updateValueAndValidity({onlySelf: true});
+        }
+      });
+    }
   }
 
   handleCancel(): void {
-    console.log('Button cancel clicked!');
+    this.listOfFormDto = {
+      id: null,
+      name : null,
+      value: null,
+      numberOfDeckers: 0
+    };
     this.isVisible = false;
   }
+getall(){
+  this.layoutService.getAll().subscribe((res: any)=>{
+    this.listOfData = res;
+ })
+}
+
+delete(record: any){
+  console.log('record',record.id);
+  this.confirmModal = this.modal.confirm({
+    nzTitle: 'Xác nhận xóa bản ghi?',
+    nzContent: 'Bạn có chắc muốn xóa bản ghi này không?',
+    nzOnOk: () =>{
+      this.layoutService.deleteLayout(record.id).subscribe((res: any)=>{
+        this.successDeleteMessage();
+        this.getall();
+     })
+    }
+  });
+}
+
+update(){
 
 }
+
+updateLayout(item: any){
+  
+}
+
+}
+
--- src/app/pages/quanlylayout/quanlylayout.service.ts
+++ src/app/pages/quanlylayout/quanlylayout.service.ts
@@ -5,8 +5,17 @@ import { ServiceBaseService } from 'src/app/services/service-base.service';
   providedIn: 'root'
 })
 export class QuanLyLayoutService extends ServiceBaseService{
- getAllByRoute(params: any){
-    return this.post(`api/Coach/GetAllByRoute`, params);
+ getAll(){
+    return this.get(`api/Layout/Getall`);
  }
+ create(params: any){
+  return this.post(`api/Layout/Create`, params);
+ }
+ deleteLayout(id: any){
+  return this.delete(`api/Layout/Delete/${id}`);
+}
+update(params: any){
+  return this.put(`/api/Layout/Update/${params.id}`, params);
+}
 
 }
